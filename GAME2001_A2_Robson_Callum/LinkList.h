8623c37861dcee02e45930cb3bbd60afc98ce8c4
Callum R <72466078+Callum-Robson@users.noreply.github.com>
2021-11-18 19:21:51 -0500

    changed Pop and Push functions to match assignment requirements

--- GAME2001_A2_Robson_Callum/LinkList.h
+++ GAME2001_A2_Robson_Callum/LinkList.h
@@ -66,7 +66,8 @@ public:
 		}
 	}
 
-	LinkNode<T>* Begin()
+	Return first node in list;
+	LinkNode<T>* Front()
 	{
 		assert(m_root != NULL);
 		return m_root;
@@ -87,51 +88,53 @@ public:
 		return m_size;
 	}
 
-	void Push(T newData)
-	{
-		LinkNode<T>* node = new LinkNode<T>;
-
-		assert(node != NULL);
-		node->m_data = newData;
-		node->m_next = NULL;
-
-		if (m_lastNode != NULL)
-		{
-			m_lastNode->m_next = node;
-			m_lastNode = node;
-		}
-		else
-		{
-			m_room = node;
-			m_lastNode = node;
-		}
-		m_size++;
-	}
-
+	//Pop first node in list
 	void Pop()
 	{
-		asser(m_root != NULL);
+		assert(M - root != NULL);
 
-		if (m_root->m_next == NULL)
-		{
-			delete m_root;
-			m_root = NULL;
-		}
-		else
-		{
-			LinkNode<T>* prevNode = m_root;
+		LinkNode<T>* temp = m_root;
 
-			while (prevNode->m_next != NULL && prevNode->m_next != m_lastNode)
-			{
-				prevNode = prevNode->m_next;
-			}
+		m_root = m_root->m_next;
 
-			delete m_lastNode;
-			prevNode->m_next = NULL;
-			m_lastNode = prevNode;
-		}
+		if (m_root != NULL)
+			m_root->m_previous = NULL;
+
+		delete temp;
 
 		m_size = (m_size == 0 ? m_size : m_size - 1);
+	}
+
+	// inserts a node before the node currently pointed to by the iterator passed in, set's the new node's data to what is passed in
+	void Push(LinkIterator<T>& it, T newData)
+	{
+		assert(it.m_node != NULL);
+
+		// create "new node"
+		LinkNode<T>* node = new LinkNode<T>;
+		assert(node != NULL);
+
+		// set "new node"'s data to what is passed in
+		node->m_data = newData;
+		// set "new node"'s next node pointer to the iterator's current node
+		node->m_next = it.m_node;
+		// set the "new node"'s previous node pointer to the iterator's previous node pointer
+		node->m_previous = it.m_node->m_previous;
+
+		// if the previous node that was just set is not null, set that node's next node pointer to the "new node"
+		if (node->m_previous != NULL)
+			node->m_previous->m_next = node;
+
+		// set the iterator's previous node pointer to the "new node"
+		it.m_node->m_previous = node;
+
+		// now all the node pointers are pointing to the right thing
+
+		// if the iterator's current node is the root node, make the "new node" the root node
+		if (it.m_node == m_root)
+			m_root = node;
+
+		m_sizee++;
 	}
 
 private:
@@ -146,7 +149,12 @@ class LinkNode
 	friend class LinkIterator<T>;
 	friend class LinkList<T>;
 
+	LinkNode() : m_priority(0), m_root(0), m_lastNode(0)
+	{
+	}
+
 private:
 	T m_data;
+	int m_priority;
 	LinkNode* m_next;
 };
